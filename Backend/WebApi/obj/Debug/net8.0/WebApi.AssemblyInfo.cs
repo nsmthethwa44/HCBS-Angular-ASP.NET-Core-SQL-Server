f117c7fa9c97dc16464ebbf860276d85cc50f0ac
Nkululeko Mthethwa <mthethwan44@gmail.com>
2025-11-08 11:42:36 +0200

    add dist folder

--- Backend/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ Backend/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09a065eedc11c0f2439629635b2894bd20161116")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b63875a5adcf7db3977fd55cc13c6471f31f50d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
