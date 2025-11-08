3873967a8b49aefbc1ac4db828e316ff94a66398
Nkululeko Mthethwa <mthethwan44@gmail.com>
2025-11-08 15:54:35 +0200

    Add CORS rules to allow requests from specific origins

--- Backend/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ Backend/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b63875a5adcf7db3977fd55cc13c6471f31f50d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bf1327941abfda6f8220959bdd109cf3dc2fb4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Backend/WebApi/obj/Release/net8.0/WebApi.AssemblyInfo.cs
+++ Backend/WebApi/obj/Release/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23aef279179c29c37722d986992d013e6d969b56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bf1327941abfda6f8220959bdd109cf3dc2fb4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
