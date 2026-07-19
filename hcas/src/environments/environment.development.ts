0e1903220a8987d3b2790e9736792026e78b5991
Nkululeko Mthethwa <mthethwan44@gmail.com>
2026-07-19 14:25:28 +0200

    Update env file

--- hcas/src/environments/environment.development.ts
+++ hcas/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
       production: true,
-     apiUrl: "http://localhost:5042"
+     apiUrl: "https://hcbs-angular-asp-net-core-sql-server.onrender.com"
 };
--- hcas/src/environments/environment.ts
+++ hcas/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: "http://localhost:5042"
+    apiUrl: "https://hcbs-angular-asp-net-core-sql-server.onrender.com"
 };
